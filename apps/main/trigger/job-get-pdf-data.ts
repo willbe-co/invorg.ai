ea9cdfbee450c231641d3640d8a622847e74ce1d
Ricardo Magalhães <ricardo@willbe.co>
2025-04-14 12:27:00 +0100

    mistral

--- apps/main/trigger/job-get-pdf-data.ts
+++ apps/main/trigger/job-get-pdf-data.ts
@@ -1,4 +1,5 @@
-import { anthropic } from "@ai-sdk/anthropic"
+// import { anthropic } from "@ai-sdk/anthropic"
+import { mistral } from "@ai-sdk/mistral"
 import { logger, task, wait } from "@trigger.dev/sdk/v3";
 import { generateObject } from "ai";
 import { z } from "zod";
@@ -24,6 +25,7 @@ export const invoiceWebhookSchema = z.object({
       amount: z.number(),
     }),
   ),
+  currency: z.string().nullable().describe("Three-lettter ISO 4217 currency code (e.g., USD, EUR)"),
   subtotalAmount: z.number(),
   taxAmount: z.number(),
   totalAmount: z.number(),
@@ -52,16 +54,16 @@ export const jobGetPdfData = task({
   run: async (payload: Props, { ctx }) => {
     const { docUrl, userId, invoiceId } = payload
 
-    const downloadResponse = await fetch(docUrl)
-
-    if (!downloadResponse.ok) {
-      throw new Error("Error download pdf")
-    }
-    const buffer = await downloadResponse.arrayBuffer()
-    const pdfBuffer = Buffer.from(buffer)
+    // const downloadResponse = await fetch(docUrl)
+    //
+    // if (!downloadResponse.ok) {
+    //   throw new Error("Error download pdf")
+    // }
+    // const buffer = await downloadResponse.arrayBuffer()
+    // const pdfBuffer = Buffer.from(buffer)
 
     const result = await generateObject<InvoiceWebhookData>({
-      model: anthropic("claude-3-5-haiku-latest"),
+      model: mistral("mistral-small-latest"),
       schema: invoiceWebhookSchema,
       messages: [
         {
@@ -69,11 +71,11 @@ export const jobGetPdfData = task({
           content: [
             {
               type: "text",
-              text: "Extract the structured invoice data from this PDF. Include all relevant fields like invoice number, dates, vendor details, client details, line items, and payment information.",
+              text: "You are an expert multilingual document parser specialized in extracting structured data from financial documents, either invoices or receipts",
             },
             {
               type: "file",
-              data: pdfBuffer,
+              data: new URL(docUrl),
               mimeType: "application/pdf",
             },
           ],
