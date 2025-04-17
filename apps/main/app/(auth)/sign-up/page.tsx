ac0ce0bed7d5dfd2062c16a46338e20d14de0edc
Ricardo Magalhães <ricardo@willbe.co>
2025-04-17 17:21:02 +0100

    link to home

--- apps/main/app/(auth)/sign-up/page.tsx
+++ apps/main/app/(auth)/sign-up/page.tsx
@@ -2,6 +2,7 @@ import GridBackground from "@/components/grid-background";
 import { MainLogo } from "@/components/main-logo";
 import { MainLogoBlack } from "@/components/main-logo-black";
 import { SignUp } from "@/modules/auth/components/sign-up";
+import Link from "next/link";
 import { Metadata } from "next/types";
 
 export const metadata: Metadata = {
@@ -15,9 +16,11 @@ export default function SignUpPage() {
       <GridBackground />
       <div className="max-w-xl p-4 mx-auto min-h-screen flex items-center">
         <div className="w-full flex flex-col space-y-6">
-          <div className="max-w-52 mx-auto w-full">
-            <MainLogo />
-          </div>
+          <Link href="/">
+            <div className="max-w-52 mx-auto w-full">
+              <MainLogo />
+            </div>
+          </Link>
           <SignUp />
         </div>
       </div>
